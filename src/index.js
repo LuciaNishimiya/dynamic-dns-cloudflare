c167c5ef327e765bcc1892763a707c69d8b3e925
LuciaNishimiya <lucianishimiya@gmail.com>
2023-11-13 00:40:55 -0300

    Update ip if I change every 1 minute

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import { getIp } from "./services/getIpApi";
+import dotenv from "dotenv";
+import { updateDns } from "./services/cloudflareApi";
+import { getIp } from './services/getIpApi';
+import { ping } from "./services/ping";
+dotenv.config();
+let ip;
+setInterval(async () => {
 
-getIp()
+    try {
+        const serverPing = await ping(ip)
+        if (!serverPing) {
+            const newIp = await getIp();
+            if (newIp !== ip) {
+                ip = newIp;
+                await updateDns(ip)
+            }
+        }
+
+    } catch (error) {
+        console.error('Error getting IP: ' + error.message);
+    }
+}, 1 * 60 * 1000);
