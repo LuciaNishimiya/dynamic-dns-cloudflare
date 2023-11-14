a90f485595b11b38136d8aad3ad5e57a724f4aa1
LuciaNishimiya <lucianishimiya@gmail.com>
2023-11-13 23:43:10 -0300

    improved last ip verification and sending notifications to discord

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
-import dotenv from "dotenv";
+import { sendMessage } from "./services/discord";
 import { updateDns } from "./services/cloudflareApi";
 import { getIp } from './services/getIpApi';
 import { ping } from "./services/ping";
+import dotenv from 'dotenv';
+import { getDnsData } from "./services/cloudflareApi/getDnsData";
 dotenv.config();
-let ip;
+
+const subdomain = process.env.CLOUDFLARE_SUBDOMAIN;
+
+async function setIP(ip) {
+    // this is only used to add styles to the discord message
+    const backticks = '`'
+    //
+    await updateDns(ip)
+    await sendMessage(
+        `>>> ## :arrows_counterclockwise:  El servidor cambió de IP: ${backticks}${ip}${backticks}\n
+## :white_check_mark: ${backticks}${subdomain}${backticks} actualizado correctamente`
+    )
+}
+async function onRestart() {
+    await sendMessage(
+        '# :warning:  El servidor se ha reiniciado.'
+    )
+    const newIp = await getIp();
+    const dnsData = await getDnsData()
+    const dnsIp = dnsData.content;
+
+    if (dnsIp !== newIp) {
+        setIP(newIp);
+    }
+}
+onRestart()
+
 setInterval(async () => {
 
     try {
-        const serverPing = await ping(ip)
-        if (!serverPing) {
-            const newIp = await getIp();
-            if (newIp !== ip) {
+        const newIp = await getIp();
+        const dnsData = await getDnsData()
+        const dnsIp = dnsData.content;
+
+        if (dnsIp !== newIp) {
                 ip = newIp;
-                await updateDns(ip)
-            }
+            setIP(newIp);
         }
 
     } catch (error) {
         console.error('Error getting IP: ' + error.message);
     }
-}, 1 * 60 * 1000);
+}, 20 * 60 * 1000);
